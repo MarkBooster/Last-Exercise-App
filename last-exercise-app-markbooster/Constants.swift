46dda68f6bdf65193346c9acd1e54567f0edf463
Mark Booster <markbooster@MacBook-Pro-van-Mark-2.local>
2016-05-22 17:25:30 +0200

    Created Login Interface

---
+++ last-exercise-app-markbooster/Constants.swift
@@ -0,0 +1,12 @@
+//
+//  Constants.swift
+//  last-exercise-app-markbooster
+//
+//  Created by Mark Booster on 22-05-16.
+//  Copyright © 2016 Mark Booster. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+let SHADOW_COLOR: CGFloat = 157.0 / 255.0 //When you want a color always divide it with 255.0
