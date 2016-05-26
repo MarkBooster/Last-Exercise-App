864c1b453d0eeadacda54d7e016f34d3c7064e30
Mark Booster <markbooster@MacBook-Pro-van-Mark-2.local>
2016-05-26 20:31:39 +0200

    Worked on the front-end of the app

---
+++ last-exercise-app-markbooster/FeedVC.swift
@@ -0,0 +1,39 @@
+//
+//  FeedVC.swift
+//  last-exercise-app-markbooster
+//
+//  Created by Mark Booster on 26-05-16.
+//  Copyright © 2016 Mark Booster. All rights reserved.
+//
+
+import UIKit
+
+class FeedVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    @IBOutlet weak var tableView: UITableView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        tableView.delegate = self
+        tableView.dataSource = self
+
+
+    }
+    
+    func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 3
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        return tableView.dequeueReusableCellWithIdentifier("PostCell") as! PostCell
+    }
+    
+    
+
+
+}
---
+++ last-exercise-app-markbooster/PostCell.swift
@@ -0,0 +1,34 @@
+//
+//  PostCell.swift
+//  last-exercise-app-markbooster
+//
+//  Created by Mark Booster on 26-05-16.
+//  Copyright © 2016 Mark Booster. All rights reserved.
+//
+
+import UIKit
+
+class PostCell: UITableViewCell {
+    
+    @IBOutlet weak var profileImg: UIImageView!
+    @IBOutlet weak var showcaseImg: UIImageView!
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+    }
+    
+    override func drawRect(rect: CGRect) {
+        profileImg.layer.cornerRadius = profileImg.frame.size.width / 2
+        profileImg.clipsToBounds = true
+        
+        showcaseImg.clipsToBounds = true 
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
