5c45c306cbe48c58687d0ad4cd77087bb286a085
Mark Booster <markbooster@MacBook-Pro-van-Mark-2.local>
2016-06-01 13:16:57 +0200

    Created Post UI

--- last-exercise-app-markbooster/FeedVC.swift
+++ last-exercise-app-markbooster/FeedVC.swift
@@ -9,10 +9,14 @@
 import UIKit
 import Firebase
 
-class FeedVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
+class FeedVC: UIViewController, UITableViewDelegate, UITableViewDataSource, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
     
     @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var postField: MaterialTextField!
+    @IBOutlet weak var imageSelectorImage: UIImageView!
     var posts = [Post]()
+    
+    var imagePicker: UIImagePickerController!
     static var imageCache = NSCache()
 
     override func viewDidLoad() {
@@ -21,6 +25,8 @@ class FeedVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
         tableView.delegate = self
         tableView.dataSource = self
         tableView.estimatedRowHeight = 370
+        imagePicker = UIImagePickerController()
+        imagePicker.delegate = self
         
         DataService.ds.REF_POSTS.observeEventType(.Value, withBlock: { snapshot in
             print(snapshot.value)
@@ -81,7 +87,20 @@ class FeedVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
         }
     }
     
+    func imagePickerController(picker: UIImagePickerController, didFinishPickingImage image: UIImage, editingInfo: [String : AnyObject]?) {
+        imagePicker.dismissViewControllerAnimated(true, completion: nil)
+        imageSelectorImage.image = image
+    }
+    @IBAction func selectImage(sender: UITapGestureRecognizer) {
+        presentViewController(imagePicker, animated: true, completion: nil)
+        
+        
+    }
+    @IBAction func makePost(sender: AnyObject) {
+    }
+}
+    
     
 
 
-}
+
