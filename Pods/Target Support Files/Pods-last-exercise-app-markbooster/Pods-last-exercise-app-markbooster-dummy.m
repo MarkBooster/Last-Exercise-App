13de2b7a0474a600802df03b6e52763de9972a0a
Mark Booster <markbooster@MacBook-Pro-van-Mark-2.local>
2016-06-06 21:27:44 +0200

    Installed Cocoapods - Updated Firebase in app

---
+++ Pods/Target Support Files/Pods-last-exercise-app-markbooster/Pods-last-exercise-app-markbooster-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_last_exercise_app_markbooster : NSObject
+@end
+@implementation PodsDummy_Pods_last_exercise_app_markbooster
+@end
---
+++ Pods/Target Support Files/Pods-last-exercise-app-markbooster/Pods-last-exercise-app-markbooster-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double Pods_last_exercise_app_markboosterVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_last_exercise_app_markboosterVersionString[];
+
